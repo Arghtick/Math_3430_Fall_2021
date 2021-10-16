790c43880815cd3b59ae0fa795e50b9042bae0b0
Rider Jefferies <51686626+RiderJefferies@users.noreply.github.com>
2021-10-15 21:05:43 -0500

    update test_LA.py

--- test_LA.py
+++ test_LA.py
@@ -16,7 +16,7 @@ def test_add_vectors1():
   
 def test_add_vectors2():
     
-    assert LA.add_vectors(test_vector_01, test_vector_02) == [3,2,5]
+    assert LA.add_vectors(test_vector_02,test_vector_03) == [8,4,3]
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       
@@ -26,7 +26,7 @@ def test_scalar_vector_mult1():
     
 def test_scalar_vector_mult2():
     
-    assert LA.scalar_vector_mult(test_const_01, test_vector_01) == [1,3,7]
+    assert LA.scalar_vector_mult(test_const_02, test_vector_02) == [9,3,6]
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -36,7 +36,7 @@ def test_scalar_matrix_mult1():
   
 def test_scalar_matrix_mult2():
     
-    assert LA.scalar_matrix_mult(test_const_01, test_matrix_01) == [[1,2,3],[4,5,6],[7,8,9]]
+    assert LA.scalar_matrix_mult(test_const_01,test_identity_mtx) == [[2,0,0],[0,2,0],[0,0,2]]
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -44,7 +44,7 @@ def test_add_matrix1():
     assert LA.add_matrix(test_matrix_01,test_matrix_01) == [[2,4,8],[6,2,4],[10,6,2]]
 
 def test_add_matrix2():
-    assert LA.add_matrix(test_matrix_01,test_matrix_01) == [[1,2,3],[4,5,6],[7,8,9]]
+    assert LA.add_matrix(test_matrix_01,test_identity_mtx) == [[2,2,4],[3,2,2],[5,3,2]]
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     
@@ -52,7 +52,7 @@ def test_matrix_vector_mult1():
     assert LA.matrix_vector_mult(test_matrix_01,test_vector_01) == [27, 16, 12]
 
 def test_matrix_vector_mult2():
-    assert LA.matrix_vector_mult(test_matrix_01,test_vector_01) == [1, 2, 3]
+    assert LA.matrix_vector_mult(test_matrix_01,test_vector_02) == [16,13,16]
    
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    
    
@@ -60,6 +60,6 @@ def test_matrix_matrix_mult1():
     assert LA.matrix_matrix_mult(test_matrix_01, test_matrix_01) == [[27,16,12],[16,13,16],[19,16,27]]
     
 def test_matrix_matrix_mult2():
-    assert LA.matrix_matrix_mult(test_matrix_01, test_matrix_01) == [[1,2,3],[4,5,6],[7,8,9]]
+    assert LA.matrix_matrix_mult(test_matrix_01, test_identity_mtx) == [[1,2,4],[3,1,2],[5,3,1]]
     
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    
