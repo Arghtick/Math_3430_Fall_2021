a407bd6d0c7d26e0d098882eb5b3d42a63f7a875
Rider Jefferies <51686626+RiderJefferies@users.noreply.github.com>
2021-11-15 23:01:28 -0600

    Upload householder function

--- QR.py
+++ QR.py
@@ -100,3 +100,146 @@ def orthonormal_vectorset(mtx_1: list):
     return Q
 
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def householder(mtx:list):
+    """Performs Householder triangularization
+
+    Parameters
+    ----------
+    mtx : list
+        A input matrix of floats
+
+    Returns
+    -------
+    a list where first index is an orthogonal matrix Q and the second element 
+    is an upper triangular matrix R
+
+    """
+    
+    #m = len(mtx)
+    #n = len(mtx[0])
+    R:list = deep_copy(mtx)
+    Q_list: list = []
+    
+    
+    for k in range(len(R)):
+        x: list = Q_builder(R,k)
+        
+        
+        R = LA.matrix_matrix_mult(x, R)
+        
+        Q_list.append(x)
+        
+    
+    Q: list = Q_list[-1]
+    
+    Q: list = conjugate_transpose(Q_list[0])
+    
+    for index in range (1, len(Q_list)):
+        ct = conjugate_transpose(Q_list[index])
+        Q = LA.matrix_matrix_mult(Q, ct)
+    print([Q,R])
+    print("~~~~~~~~~~~~~~~~~~~~~`")
+    print(LA.matrix_matrix_mult(Q, R))
+    return[Q,R]
+
+
+
+def i_mtx(x: int)->list:
+    i_list: list = [[0 for element in range(x)] for index in range(x)]
+    for index in range(x):
+       # for element in range(x):
+        
+        i_list[index][index] = 1
+    return i_list
+  
+    
+  
+    
+  
+def sign(x: float)-> float:
+    if x>=0:
+        return 1
+    else:
+        return -1
+    
+def vec_vec_mult(vector_1, vector_2):
+    result = []
+    vector_1 == vector_2
+    for x in range(len(vector_1)):
+        result.append(LA.scalar_vector_mult(vector_1[x], vector_2))
+    return result
+    
+def F_builder(vec: list)->list:
+    s = -2/(LA.pNorm(vec))**2
+    x = LA.scalar_matrix_mult(s, vec_vec_mult(vec, vec))
+    y = LA.add_matrix(i_mtx(len(vec)), x)
+    return y
+    
+def Q_builder(mtx:list, n: int):
+    """
+    builds a iteration of the orthogonal matrix Q
+    
+    Parameters
+    ----------
+    mtx : list
+        in input matrix stored as a list of lists.
+    n : int
+        iteration index.
+
+    Returns
+    -------
+     Q 
+
+    """ 
+    
+    A:list = [[0 for j in range (n, len(mtx[i]))]for i in range(n,len(mtx))]
+    for i in range(len(mtx)):
+        for j in range(len(mtx[i])):
+            if n+i < len(mtx[i]):
+                if n+j < len(mtx[i]):
+                    A[i][j] = mtx[n+i][n+j]
+    
+    v = reflect_vec(A[0])
+    
+    f = F_builder(v)
+    Q = i_mtx(len(mtx))
+    for i in range(n,len(Q)):
+        for j in range(n,len(Q)):
+            Q[i][j] = f[i-n][j-n]
+    return Q
+    
+    
+def reflect_vec(vec: list)->list:
+    e = [0 for element in range(len(vec))]
+    e[0]=1
+    v = LA.add_vectors(LA.scalar_vector_mult(sign(vec[0])*LA.pNorm(vec), e),vec)
+    return v
+
+def deep_copy(mtx:list)-> list:
+    a: list = [[0 for element in range(len(mtx[0]))]for index in range(len(mtx))]
+    for index in range(len(mtx)):
+        for element in range(len(mtx[index])):
+            a[index][element]= mtx[index][element]
+    return a
+
+
+
+def conjugate_transpose(mtx_1: list)-> list:
+    con_mtx: list =[[0 for element in range(len(mtx_1[0]))]for index in range(len(mtx_1))]
+    tran_mtx: list = [[0 for element in range(len(mtx_1[0]))]for index in range(len(mtx_1))]
+    for i in range (len(mtx_1)):
+        for j in range(len(mtx_1)):
+            con_mtx[i][j]=(LA.conjugate(mtx_1[i][j]))
+            
+    for i in range (len(mtx_1)):
+        for j in range(len(mtx_1)):
+            tran_mtx[i][j]=(con_mtx[j][i])
+    return tran_mtx
+
+
+x = [[2,2,1],[-2,1,2],[18,0,0]]
+householder(x)
+
+    
